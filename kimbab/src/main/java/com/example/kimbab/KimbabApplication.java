bbfb4c3fe174061a24939d7e52c26a40d54d24f3
username <user@email.mail>
2022-10-22 17:39:50 +0900

    데이터의 생성날짜와 수정 날짜를 자동으로 설정하기위한 작업

--- kimbab/src/main/java/com/example/kimbab/KimbabApplication.java
+++ kimbab/src/main/java/com/example/kimbab/KimbabApplication.java
@@ -2,8 +2,10 @@ package com.example.kimbab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing	//jpa의 동작을 감시하겠다는 설정
 public class KimbabApplication {
 
 	public static void main(String[] args) {
---
+++ kimbab/src/main/java/com/example/kimbab/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.kimbab.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@MappedSuperclass	//entity로 사용은 가능하지만 테이블을 생성하지는 않음
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+public abstract class BaseEntity {	//인스턴스 생성 불가능 하도록 abstract
+	@CreatedDate
+	@Column(name = "regdate", updatable = false)
+	private LocalDateTime regDate;
+	
+	@LastModifiedDate
+	@Column(name = "mddate")
+	private LocalDateTime modDate;
+}
