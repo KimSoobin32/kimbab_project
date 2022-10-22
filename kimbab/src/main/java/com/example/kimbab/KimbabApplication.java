db6ed329247f7091266f5d79ee0a2deb5f3299fa
username <user@email.mail>
2022-10-22 16:49:59 +0900

    initialize

---
+++ kimbab/src/main/java/com/example/kimbab/KimbabApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimbab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimbabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimbabApplication.class, args);
+	}
+
+}
---
+++ kimbab/src/test/java/com/example/kimbab/KimbabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimbab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimbabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
